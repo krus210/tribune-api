74abcf1318327341062cb41c0c4983c0814d87e0
stanislav <staskorol21@gmail.com>
2020-05-27 19:43:12 +0300

    fix postResponseDto

--- src/main/kotlin/ru/korolevss/dto/PostResponseDto.kt
+++ src/main/kotlin/ru/korolevss/dto/PostResponseDto.kt
@@ -39,7 +39,7 @@ data class PostResponseDto(
                     likedByUser = likedByUser,
                     dislikedByUser = dislikedByUser,
                     isPostOfUser = isPostOfUser,
-                    statusOfUser = user.status
+                    statusOfUser = post.user.status
             )
         }
     }
