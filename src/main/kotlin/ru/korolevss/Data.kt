58555b66b71e3b159e044e3c7e29de2cec9f3734
stanislav <staskorol21@gmail.com>
2020-06-17 18:31:39 +0300

    add format of attachmentImage in Data

--- src/main/kotlin/ru/korolevss/Data.kt
+++ src/main/kotlin/ru/korolevss/Data.kt
@@ -6,13 +6,13 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 val users = mutableListOf(
-        UserModel(1, "user1", "123456", mutableListOf(25,23,21,19,17), attachmentImage = "user1", likes = 9, dislikes = 0),
-        UserModel(2, "user2", "123456", mutableListOf(24,22,20,18,16), attachmentImage ="user2", likes = 12, dislikes = 0),
-        UserModel(3, "user3", "123456", mutableListOf(15,13,11,9,7), attachmentImage ="user3", likes = 7, dislikes = 0),
-        UserModel(4, "user4", "123456", mutableListOf(14,12,10,8,6), attachmentImage ="user4", likes = 6, dislikes = 1),
-        UserModel(5, "user5", "123456", mutableListOf(5), attachmentImage ="user5", likes = 9, dislikes = 4),
-        UserModel(6, "user6", "123456", mutableListOf(4), attachmentImage ="user6", likes = 3, dislikes = 7),
-        UserModel(7, "user7", "123456", mutableListOf(3), attachmentImage ="user7", likes = 3, dislikes = 8),
+        UserModel(1, "user1", "123456", mutableListOf(25,23,21,19,17), attachmentImage = "user1.jpg", likes = 9, dislikes = 0),
+        UserModel(2, "user2", "123456", mutableListOf(24,22,20,18,16), attachmentImage ="user2.jpg", likes = 12, dislikes = 0),
+        UserModel(3, "user3", "123456", mutableListOf(15,13,11,9,7), attachmentImage ="user3.jpg", likes = 7, dislikes = 0),
+        UserModel(4, "user4", "123456", mutableListOf(14,12,10,8,6), attachmentImage ="user4.jpg", likes = 6, dislikes = 1),
+        UserModel(5, "user5", "123456", mutableListOf(5), attachmentImage ="user5.jpg", likes = 9, dislikes = 4),
+        UserModel(6, "user6", "123456", mutableListOf(4), attachmentImage ="user6.jpg", likes = 3, dislikes = 7),
+        UserModel(7, "user7", "123456", mutableListOf(3), attachmentImage ="user7.jpg", likes = 3, dislikes = 8),
         UserModel(8, "user8", "123456", mutableListOf(2), likes = 1, dislikes = 9),
         UserModel(9, "user9", "123456", mutableListOf(1), likes = 1, dislikes = 8),
         UserModel(10, "user10", "123456", mutableListOf(), likes = 0, dislikes = 12),
@@ -79,84 +79,84 @@ val dates1 = listOf(
 )
 
 val posts = mutableListOf(
-        PostModel(25, users[0].id, dates0[24], "Post 25 from user 1", "post25",
+        PostModel(25, users[0].id, dates0[24], "Post 25 from user 1", "post25.jpg",
                 likeUsersId = mutableMapOf(2L to dates1[0], 3L to dates1[1], 4L to dates1[2], 5L to dates1[3], 6L to dates1[4], 7L to dates1[5], 8L to dates1[6], 9L to dates1[7]),
                 dislikeUsersId = mutableMapOf(10L to dates1[8], 11L to dates1[9])
         ),
-        PostModel(23, users[0].id, dates0[22], "Post 23 from user 1", "post23",
+        PostModel(23, users[0].id, dates0[22], "Post 23 from user 1", "post23.jpg",
                 likeUsersId = mutableMapOf(2L to dates1[1], 3L to dates1[3], 4L to dates1[4], 5L to dates1[15]),
                 dislikeUsersId = mutableMapOf(10L to dates1[14], 11L to dates1[16], 6L to dates1[6], 7L to dates1[8], 8L to dates1[20], 9L to dates1[19])
         ),
-        PostModel(21, users[0].id, dates0[20], "Post 21 from user 1", "post21",
+        PostModel(21, users[0].id, dates0[20], "Post 21 from user 1", "post21.jpg",
                 likeUsersId = mutableMapOf(2L to dates1[4], 3L to dates1[5], 4L to dates1[6], 5L to dates1[7], 6L to dates1[8]),
                 dislikeUsersId = mutableMapOf(10L to dates1[9], 11L to dates1[10], 7L to dates1[11], 8L to dates1[8], 9L to dates1[12])
         ),
-        PostModel(19, users[0].id, dates0[18], "Post 19 from user 1", "post19",
+        PostModel(19, users[0].id, dates0[18], "Post 19 from user 1", "post19.jpg",
                 likeUsersId = mutableMapOf(2L to dates1[15], 3L to dates1[16], 4L to dates1[17], 5L to dates1[18]),
                 dislikeUsersId = mutableMapOf(10L to dates1[2], 11L to dates1[0], 7L to dates1[1], 8L to dates1[2], 9L to dates1[12])
         ),
-        PostModel(17, users[0].id, dates0[16], "Post 17 from user 1", "post17",
+        PostModel(17, users[0].id, dates0[16], "Post 17 from user 1", "post17.jpg",
                 likeUsersId = mutableMapOf(2L to dates1[4], 3L to dates1[5], 4L to dates1[6], 5L to dates1[7]),
                 dislikeUsersId = mutableMapOf(10L to dates1[9], 11L to dates1[10], 7L to dates1[11], 8L to dates1[8], 9L to dates1[12])
         ),
-        PostModel(24, users[1].id, dates0[23], "Post 24 from user 2", "post24",
+        PostModel(24, users[1].id, dates0[23], "Post 24 from user 2", "post24.jpg",
                 dislikeUsersId = mutableMapOf(10L to dates1[9], 11L to dates1[10], 7L to dates1[11], 8L to dates1[8], 9L to dates1[12])
         ),
-        PostModel(22, users[1].id, dates0[21], "Post 22 from user 2", "post22",
+        PostModel(22, users[1].id, dates0[21], "Post 22 from user 2", "post22.jpg",
                 dislikeUsersId = mutableMapOf(5L to dates1[15], 6L to dates1[16], 7L to dates1[17], 8L to dates1[8], 9L to dates1[0], 10L to dates1[4], 11L to dates1[5])
         ),
-        PostModel(20, users[1].id, dates0[19], "Post 20 from user 2", "post20",
+        PostModel(20, users[1].id, dates0[19], "Post 20 from user 2", "post20.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[20], 3L to dates1[15])
         ),
-        PostModel(18, users[1].id, dates0[17], "Post 18 from user 2", "post18",
+        PostModel(18, users[1].id, dates0[17], "Post 18 from user 2", "post18.jpg",
                 dislikeUsersId = mutableMapOf(5L to dates1[15], 6L to dates1[16])
         ),
-        PostModel(16, users[1].id, dates0[15], "Post 16 from user 2", "post16",
+        PostModel(16, users[1].id, dates0[15], "Post 16 from user 2", "post16.jpg",
                 likeUsersId = mutableMapOf(5L to dates1[1]),
                 dislikeUsersId = mutableMapOf(6L to dates1[16], 7L to dates1[17], 8L to dates1[8], 9L to dates1[0], 10L to dates1[4], 11L to dates1[5])
         ),
-        PostModel(15, users[2].id, dates0[14], "Post 15 from user 3", "post15",
+        PostModel(15, users[2].id, dates0[14], "Post 15 from user 3", "post15.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[15], 2L to dates1[9], 5L to dates1[1]),
                 dislikeUsersId = mutableMapOf(11L to dates1[10])
         ),
-        PostModel(13, users[2].id, dates0[12], "Post 13 from user 3", "post13",
+        PostModel(13, users[2].id, dates0[12], "Post 13 from user 3", "post13.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[1], 2L to dates1[2], 4L to dates1[3])
         ),
-        PostModel(11, users[2].id, dates0[10], "Post 11 from user 3", "post11",
+        PostModel(11, users[2].id, dates0[10], "Post 11 from user 3", "post11.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[15], 2L to dates1[9], 5L to dates1[1], 7L to dates1[20]),
                 dislikeUsersId = mutableMapOf(6L to dates1[10], 11L to dates1[8])
         ),
-        PostModel(9, users[2].id, dates0[8], "Post 9 from user 3", "post9",
+        PostModel(9, users[2].id, dates0[8], "Post 9 from user 3", "post9.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[15], 2L to dates1[9], 5L to dates1[1], 7L to dates1[20]),
                 dislikeUsersId = mutableMapOf(6L to dates1[10], 11L to dates1[8])
         ),
-        PostModel(7, users[2].id, dates0[6], "Post 7 from user 3", "post7",
+        PostModel(7, users[2].id, dates0[6], "Post 7 from user 3", "post7.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[0], 2L to dates1[10])
         ),
-        PostModel(14, users[3].id, dates0[13], "Post 14 from user 4", "post14",
+        PostModel(14, users[3].id, dates0[13], "Post 14 from user 4", "post14.jpg",
                 dislikeUsersId = mutableMapOf(4L to dates1[10], 5L to dates1[8],7L to dates1[3], 8L to dates1[23], 10L to dates1[15], 11L to dates1[15])
         ),
-        PostModel(12, users[3].id, dates0[11], "Post 12 from user 4", "post12",
+        PostModel(12, users[3].id, dates0[11], "Post 12 from user 4", "post12.jpg",
                 dislikeUsersId = mutableMapOf(10L to dates1[3], 11L to dates1[2])
         ),
-        PostModel(10, users[3].id, dates0[9], "Post 10 from user 4", "post10",
+        PostModel(10, users[3].id, dates0[9], "Post 10 from user 4", "post10.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[4], 2L to dates1[2]),
                 dislikeUsersId = mutableMapOf(6L to dates1[10], 10L to dates1[15], 11L to dates1[9])
         ),
-        PostModel(8, users[3].id, dates0[7], "Post 8 from user 4", "post8",
+        PostModel(8, users[3].id, dates0[7], "Post 8 from user 4", "post8.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[19], 2L to dates1[17], 3L to dates1[4], 6L to dates1[18]),
                 dislikeUsersId = mutableMapOf(5L to dates1[10])
         ),
-        PostModel(6, users[3].id, dates0[5], "Post 6 from user 4", "post6",
+        PostModel(6, users[3].id, dates0[5], "Post 6 from user 4", "post6.jpg",
                 dislikeUsersId = mutableMapOf(5L to dates1[1], 8L to dates1[20], 9L to dates1[2], 10L to dates1[15], 11L to dates1[9])
         ),
-        PostModel(5, users[4].id, dates0[4], "Post 5 from user 5", "post5",
+        PostModel(5, users[4].id, dates0[4], "Post 5 from user 5", "post5.jpg",
                 likeUsersId = mutableMapOf(1L to dates1[23])
                 ),
-        PostModel(4, users[5].id, dates0[3], "Post 4 from user 6", "post4"),
-        PostModel(3, users[6].id, dates0[2], "Post 3 from user 7", "post3"),
-        PostModel(2, users[7].id, dates0[1], "Post 2 from user 8", "post2"),
-        PostModel(1, users[8].id, dates0[0], "Post 1 from user 9", "post1")
+        PostModel(4, users[5].id, dates0[3], "Post 4 from user 6", "post4.jpg"),
+        PostModel(3, users[6].id, dates0[2], "Post 3 from user 7", "post3.jpg"),
+        PostModel(2, users[7].id, dates0[1], "Post 2 from user 8", "post2.jpg"),
+        PostModel(1, users[8].id, dates0[0], "Post 1 from user 9", "post1.jpg")
         )
 
 
